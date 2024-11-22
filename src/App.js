ecdd38ac2166c2fd499d44652eb8f38aaa5d9258
Balaji3355 <balajibandaru41@gmail.com>
2024-11-22 09:20:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CartPage from './components/CartPage';
+import Homepage from './components/Homepage';
+import LoginPage from './components/LoginPage';
+import ProductsPage from './components/ProductsPage';
+import SignUpPage from './components/SignupPage';
+import WishlistPage from './components/WishlistPage';
+import ShortsPage from './components/ShortsPage';
+import OrderPage from './components/OrderPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+         <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/wishlist" element={<WishlistPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/shorts" element={<ShortsPage />} />
+          <Route path="/order" element={<OrderPage />} />
+
+          
+
+
+        </Routes>
+    </Router>
   );
 }
 
